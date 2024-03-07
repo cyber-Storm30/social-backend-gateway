765a5923fe6ba1d0c41d7f88c3f16574f4123523
cyber-Storm30 <babairanajit@gmail.com>
2024-03-07 17:53:18 +0530

    added all hosted urls

--- index.js
+++ index.js
@@ -18,16 +18,52 @@ app.use(express.urlencoded({ extended: true }));
 
 const proxyOptions = { timeout: 300000 };
 
-app.use("/message", proxy("http://localhost:8007", proxyOptions));
-app.use("/chat", proxy("http://localhost:8006", proxyOptions));
-app.use("/comment", proxy("http://localhost:8005", proxyOptions));
-app.use("/like", proxy("http://localhost:8004", proxyOptions));
-app.use("/post", proxy("http://localhost:8003", proxyOptions));
-app.use("/connection", proxy("http://localhost:8002", proxyOptions));
-app.use("/auth", proxy("http://localhost:8001", proxyOptions));
-app.use("/", (req, res) => {
-  res.status(200).json("Oops Nothing here");
-}); // auth
+const mode = "dev";
+
+if (mode === "dev") {
+  app.use("/message", proxy("http://localhost:8007", proxyOptions));
+  app.use("/chat", proxy("http://localhost:8006", proxyOptions));
+  app.use("/comment", proxy("http://localhost:8005", proxyOptions));
+  app.use("/like", proxy("http://localhost:8004", proxyOptions));
+  app.use("/post", proxy("http://localhost:8003", proxyOptions));
+  app.use("/connection", proxy("http://localhost:8002", proxyOptions));
+  app.use("/auth", proxy("http://localhost:8001", proxyOptions));
+  app.use("/", (req, res) => {
+    res.status(200).json("Oops Nothing here");
+  });
+} else {
+  app.use(
+    "/message",
+    proxy("https://social-backend-message.onrender.com", proxyOptions)
+  );
+  app.use(
+    "/chat",
+    proxy("https://social-backend-chat.onrender.com", proxyOptions)
+  );
+  app.use(
+    "/comment",
+    proxy("https://social-backend-comment.onrender.com", proxyOptions)
+  );
+  app.use(
+    "/like",
+    proxy("https://social-backend-like.onrender.com", proxyOptions)
+  );
+  app.use(
+    "/post",
+    proxy("https://social-backend-post.onrender.com", proxyOptions)
+  );
+  app.use(
+    "/connection",
+    proxy("https://social-backend-connection.onrender.com", proxyOptions)
+  );
+  app.use(
+    "/auth",
+    proxy("https://social-backend-auth.onrender.com", proxyOptions)
+  );
+  app.use("/", (req, res) => {
+    res.status(200).json("Oops Nothing here");
+  });
+}
 
 app.listen(8000, () => {
   console.log("Gateway is Listening to Port 8000");
